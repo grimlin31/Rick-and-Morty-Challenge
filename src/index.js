f0a679cb5e8e85e3f813b953c5c02b9b67b39277
grimlin31 <nico.bernal.al@gmail.com>
2022-08-06 11:24:50 -0400

    -add: function question b

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { getFromApi } from "./requests.js";
 import { countCharInString } from "./string.utils.js";
 
@@ -14,6 +15,38 @@ const countCharInNameResource = async (resource, parameter) => {
         resource
     }
     return await countCharInNameResource(resource, { count, char, page: page + 1 })
+}
+
+const originCharacterByEpisode = async (parameter) => {
+    const { page, resultObj, mapCharacter } = {...parameter};
+    const { results, info } = await getFromApi('episode', { page }).catch(e => {throw e})
+
+    results.map(async (episode) => {
+        const originLocations = []
+        for( const urlChar of episode.characters) {
+            const characterLocation = mapCharacter.get(urlChar)
+            if (!characterLocation){
+                const newChar = await (await axios.get(urlChar)).data;
+                const newCharacLoc = newChar.origin.name
+                mapCharacter.set(urlChar, newCharacLoc);
+                originLocations.push(newCharacLoc)
+                continue;
+            }
+            originLocations.push(characterLocation);
+        };
+        const locations = [...new Set(originLocations)]
+       resultObj.push({
+            name: episode.name,
+            episode: episode.episode,
+            locations,
+            locationsAmount: originLocations.length,
+        })
+    })
+
+    if ( !info?.next ) return resultObj
+
+    return await originCharacterByEpisode({ page: page + 1, resultObj, mapCharacter });
+
 }
 
 const dateStart = Date.now();
@@ -33,4 +66,5 @@ Promise.all([
 
 })
 .catch(e => console.log(e.message))
+originCharacterByEpisode({ page: 1, resultObj: [], mapCharacter: new Map() })
 
